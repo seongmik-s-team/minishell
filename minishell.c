3db4dd5431058472088cbbba821dfb06dfc09659
fing9 <djajsl54@naver.com>
2023-12-24 15:51:57 +0900

    [SETTING] 기본 프로젝트 템플릿 추가

---
+++ minishell.c
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minishell.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: seongmik <seongmik@student.42seoul.kr>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/12/24 15:45:20 by seongmik          #+#    #+#             */
+/*   Updated: 2023/12/24 15:47:12 by seongmik         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "includes/minishell.h"
+
+void	main(int argc, char *argv[], char *envp[])
+{
+	int		i;
+	char	**env;
+
+	i = 0;
+	env = envp;
+	while (env[i])
+	{
+		write(1, env[i], ft_strlen(env[i]));
+		write(1, "\n", 1);
+		i++;
+	}
+}
