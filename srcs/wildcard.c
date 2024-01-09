e80ebdbb2bc1a45e9e4b51df5369aecd2f9cd81e
Seongmin Kim <seongmik@c3r8s5.42seoul.kr>
2024-01-09 18:15:09 +0900

    [FEATURE] wildcard 구현 초기 코드 추가

--- srcs/wildcard.c
+++ srcs/wildcard.c
@@ -6,10 +6,13 @@
 /*   By: seongmik <seongmik@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/09 18:11:16 by seongmik          #+#    #+#             */
-/*   Updated: 2024/01/09 18:12:26 by seongmik         ###   ########.fr       */
+/*   Updated: 2024/01/09 18:15:01 by seongmik         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "../includes/minishell.h"
+
+// convert_wildcard() 는 일반 string에 포함된 wildcard를 공백 하나로 구분된 일반 문자열로 치환하는 함수이다.
 char	*convert_wildcard(char *str)
 {
 	char	*ret;
